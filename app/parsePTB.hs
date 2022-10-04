1651f087eeff6f4e10b4ecff67241a708175767d
Daisuke Bekki <bekki@is.ocha.ac.jp>
2022-10-04 23:32:51 +0900

    fixed minor bugs

--- app/parsePTB.hs
+++ app/parsePTB.hs
@@ -1,17 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import System.IO (hPutStrLn,stderr)  --base
+import qualified System.Environment as E --base
 import qualified Data.Text as T      --text
 import qualified Data.Text.IO as T   --text
 import Text.PTB (parsePTBfromDirectory,ptbTree2sentence) --nlp-tools
 
+-- | Usage: stack run parsePTB -- <path/to/dir> "mrg"
+
 main :: IO ()
 main = do
-  --ptbs < parsePTBfromDirectory "/home/bekki/dropbox/Public/Data/ABCbank/treebank" "psd"
-  ptbs <- parsePTBfromDirectory "/home/bekki/dropbox/Public/Data/PennTreebank/treebank_3/" "mrg"
+  (filepath:(ext:_)) <- E.getArgs
+  ptbs <- parsePTBfromDirectory filepath ext
   let sample = take 1 $ drop 2 ptbs
   mapM_ (putStrLn . show) sample
   T.putStrLn $ ptbTree2sentence $ head sample
-  --hPutStrLn stderr $ (show (length ptbs)) ++ " files processed." 
-
+  hPutStrLn stderr $ (show (length ptbs)) ++ " files processed." 
   
